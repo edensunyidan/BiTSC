d89862dc2bab5f7cd5480c9f41eccda40f25b6f6
EdenSun <43682671+edensunyidan@users.noreply.github.com>
2019-09-26 09:55:06 -0700

    Delete __init__.py

--- simulation/__init__.py
+++
@@ -1,7 +0,0 @@
-"""An empty __init__.py"""
-
-print("simulation package is getting imported!")
-
-if __name__ == "__main__":
-    # execute only if run as a script
-    print("executing the simulation __init__.py")
