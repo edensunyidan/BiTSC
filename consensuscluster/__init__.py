1a8f7d908dca756d19d10281a585b9ee94d787c6
EdenSun <43682671+edensunyidan@users.noreply.github.com>
2019-10-17 20:09:09 -0700

    Delete __init__.py

--- consensuscluster/__init__.py
+++
@@ -1,7 +0,0 @@
-"""An empty __init__.py"""
-
-print("consensuscluster package is getting imported!")
-
-if __name__ == "__main__":
-    # execute only if run as a script
-    print("executing the consensuscluster __init__.py")
