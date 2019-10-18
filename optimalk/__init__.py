f4bd64154ab7c44ae9c50989d990b2ae0d7bd5e5
EdenSun <43682671+edensunyidan@users.noreply.github.com>
2019-10-17 20:13:11 -0700

    Delete __init__.py

--- optimalk/__init__.py
+++
@@ -1,7 +0,0 @@
-"""An empty __init__.py"""
-
-print("optimalk package is getting imported!")
-
-if __name__ == "__main__":
-    # execute only if run as a script
-    print("executing the optimalk __init__.py")
