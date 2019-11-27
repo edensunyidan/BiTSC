b5a3e77951000ea9e4af5fd4fee3e222f78e5455
EdenSun <43682671+edensunyidan@users.noreply.github.com>
2019-11-27 10:05:00 -0800

    Add files via upload

---
+++ Simulation/simdata/__init__.py
@@ -0,0 +1,7 @@
+"""An empty __init__.py"""
+
+print("simdata package is getting imported!")
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    print("executing the simdata __init__.py")
