d1dc98fc849a0000f0773b59ed4c62927aa72750
EdenSun <43682671+edensunyidan@users.noreply.github.com>
2020-05-20 00:15:03 -0700

    Update BiTSC.py

--- BiTSC.py
+++ BiTSC.py
@@ -1,10 +1,10 @@
+import argparse
 import sys
 import os
 
-import pandas as pd
-import re
+from scipy.special import comb
 import numpy as np
-from biptightkmeans.cluster import BitKmeans
+from biptightkmeans.cluster_github import BitKmeans
 
 import json
 import gc
@@ -12,58 +12,79 @@ import gc
 
 if __name__ == "__main__":
     
-    #input data file, absolute path
+    parser = argparse.ArgumentParser(description='Parse the command-line arguments as the input for BiTSC.')
+    #group = parser.add_mutually_exclusive_group()
+    
+    parser.add_argument('--covariate', nargs='*', required=True, type=str)
+    parser.add_argument('--edge', nargs='*', required=True, type=str)
+    parser.add_argument('--k', nargs='*', type=int, default=[5, 50], dest='k_vec')
 
-    linkage_filepath = sys.argv[1] # .txt
-    sideonedata_filepath = sys.argv[2] # .mx
-    sidetwodata_filepath = sys.argv[3] # .mx
     
-    linkage_data = pd.read_csv(linkage_filepath) #, sep='\t'
-    sideone_raw_data = pd.read_csv(sideonedata_filepath) #, sep='\t'
-    sidetwo_raw_data = pd.read_csv(sidetwodata_filepath) #, sep='\t'
+    parser.add_argument('--kernel', default='rbf', type=str, help="Metric used when calculating kernel. Valid values for metric are: {'additive_chi2', 'chi2', 'linear', 'poly', 'polynomial', 'rbf', 'laplacian', 'sigmoid', 'cosine'}")
+    parser.add_argument('--tau', nargs='*', type=float, default=None, action='append', dest='tao_vec') 
+    parser.add_argument('--rho', type=float, default=0.8, dest='rho')
+    parser.add_argument('--ncore', type=int, default=10, dest='num_core')
+    parser.add_argument('--niter', type=int, default=100, dest='num_iteration')
     
-    tao = 1
-    drop=True
-    kernel = True
+
+    parser.add_argument('--alpha', nargs='*', type=float, default=[0.90, 0.95, 1.00], dest='alpha_vec')
+    parser.add_argument('--heatmap', default=False)
+    parser.add_argument('--threshold', type=int, default=10, dest='thre_num_node')
+    parser.add_argument('--output_dir', nargs=1, type=str, default='./', dest='root_dir_savedata')
     
-    samp_p = float(sys.argv[8])
+    parser.print_help()
+    args = parser.parse_args()
+
+    print(args.covariate)
+    print(args.edge)
+    print(args.k_vec)
+    print(args.kernel)
+    print(args.tao_vec)
+    print(args.rho)
+    print(args.num_core)
+    print(args.num_iteration)
+    print(args.alpha_vec)
+    print(args.heatmap)
+    print(args.thre_num_node)
+    print(args.root_dir_savedata)
     
-    bitkmeans = BitKmeans(left_raw_data=sideone_raw_data, right_raw_data=sidetwo_raw_data, orthologs_data=linkage_data,
-                          samp_p=samp_p, tao=tao, drop=drop, kernel=kernel)
-        
-    k_min = int(sys.argv[4])
-    k_max = int(sys.argv[5])
+
+    if len(args.edge) != comb(len(args.covariate), 2):
+        print("edge does not have the correct length: ", comb(len(args.covariate), 2))
+        sys.exit()
+
+    if args.tao_vec is None:
+        tao_vec = len(args.edge)*[[1,1]]
+    elif len(args.tao_vec) == comb(len(args.covariate), 2)*2:
+        tao_vec = list(zip(tao_vec[::2], tao_vec[1::2]))
+    else:
+        print("tao does not have the correct length: ", comb(len(args.covariate), 2)*2)
+        sys.exit()
+
+
+    if len(args.k_vec) not in [1,2]:
+        print("k does not have the correct length")
+        sys.exit()
+       
     
-    resamp_num = int(sys.argv[6])
-    iteration = int(int(sys.argv[7])/resamp_num)
-    print("resamp_num: ", resamp_num)
-    print("iteration: ", iteration)
-    
-    one_thre = int(sys.argv[9])
-    two_thre = int(sys.argv[10])
-    
-    root_dir_savedata = sys.argv[11]
-    
-    arguments = len(sys.argv) - 1
-    alpha_vec = []
-    
-    for i in range(12, arguments+1):
-        alpha = float(sys.argv[i])
-        alpha_vec.append(alpha)
+    bitkmeans = BitKmeans(covariate_data_path=args.covariate, edge_data_path=args.edge, 
+        kernel_type=args.kernel, samp_p=args.rho, tao=tao_vec)
     
     try:
-        os.mkdir(root_dir_savedata)
+        os.mkdir(args.root_dir_savedata)
     except FileExistsError:
         pass
 
-    output_hc = bitkmeans.fit_hierarchical_clustering(k_min=k_min, k_max=k_max, alpha_vec=alpha_vec, plot_root_dir=root_dir_savedata,
-                                                      thre_min_cluster_left=one_thre, thre_min_cluster_right=two_thre,
-                                                      iteration=iteration, resamp_num=resamp_num, delta=0.05)
-    for alpha in alpha_vec:
-        with open(root_dir_savedata+ "/cluster_alpha"+str(alpha)+".txt", 'w') as file_object:
-            for sub_tclust_id in output_hc[alpha]['tclust_id']:
+    output_hc = bitkmeans.fit_hierarchical_clustering(k_vec=args.k_vec, alpha_vec=args.alpha_vec, 
+        iteration=int(args.num_iteration/args.num_core), resamp_num=args.num_core, thre_num_node=args.thre_num_node, 
+        plot_root_dir=args.root_dir_savedata, heatmap=args.heatmap)
+
+    for alpha in args.alpha_vec:
+        with open(args.root_dir_savedata+ "/result_alpha_"+str(alpha)+".txt", 'w') as file_object:
+            for sub_tclust_id in output_hc[alpha]:
                 file_object.write("cluster_size:"+str(len(sub_tclust_id))+"\t")
                 file_object.write(",".join(sub_tclust_id)+"\n")
     
     del output_hc
     gc.collect()
+
