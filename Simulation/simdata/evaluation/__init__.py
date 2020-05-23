02d6fb0c20a58011408d10465b933d310881fc82
EdenSun <43682671+edensunyidan@users.noreply.github.com>
2020-05-23 00:52:15 -0700

    Delete __init__.py

--- Simulation/simdata/evaluation/__init__.py
+++
@@ -1,7 +0,0 @@
-"""An empty __init__.py"""
-
-print("evaluation package is getting imported!")
-
-if __name__ == "__main__":
-    # execute only if run as a script
-    print("executing the evaluation __init__.py")
